015f4baffc6a042b83d429f68812e1aab8f60dc6
Baby Indra Nandagopal <nnandagopal@yahoo.com>
2022-09-08 13:41:34 -0400

    updated df to sql statements

--- project2.sql
+++ project2.sql
@@ -1,34 +1,9 @@
--- DB schema for project2 database.
-DROP TABLE IF EXISTS USA_WVS_2017;
-DROP TABLE IF EXISTS Russia_WVS_2017;
-DROP TABLE IF EXISTS WHS_2017;
+--DROP TABLE IF EXISTS public."Organizational_Membership_2017";
+--DROP TABLE IF EXISTS public."Political_Participation_2017";
+--DROP TABLE IF EXISTS public."WHS_2017";
+--DROP TABLE IF EXISTS public.social_values_2017;
 
-CREATE TABLE USA_WVS_2017 (
-    country VARCHAR   NOT NULL,
-    
-    
-    
-    title VARCHAR   NOT NULL,
-    CONSTRAINT pk_titles PRIMARY KEY (
-        title_id
-     )
-);
-
-CREATE TABLE Russia_WVS_2017 (
-    country VARCHAR   NOT NULL,
-    
-    hire_date DATE   NOT NULL,
-    CONSTRAINT pk_employees PRIMARY KEY (
-        emp_no
-     )
-);
-
-CREATE TABLE WHS_2017 (
-    country VARCHAR   NOT NULL,
-    
-    
-    
-    CONSTRAINT pk_salaries PRIMARY KEY (
-        emp_no
-     )
-);
+select * from public."Organizational_Membership_2017";
+--select count(*) from public."Political_Participation_2017";
+--select count(*) from public."WHS_2017";
+--select count(*) from public.social_values_2017;
