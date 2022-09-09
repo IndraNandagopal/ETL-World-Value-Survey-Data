208eadc282568ea8d2a20d4b169701779c3122d1
Baby Indra Nandagopal <nnandagopal@yahoo.com>
2022-09-08 20:11:27 -0400

    cleaned up files

--- project2.sql
+++
@@ -1,9 +0,0 @@
---DROP TABLE IF EXISTS public."Organizational_Membership_2017";
---DROP TABLE IF EXISTS public."Political_Participation_2017";
---DROP TABLE IF EXISTS public."WHS_2017";
---DROP TABLE IF EXISTS public.social_values_2017;
-
-select * from public."Organizational_Membership_2017";
---select count(*) from public."Political_Participation_2017";
---select count(*) from public."WHS_2017";
---select count(*) from public.social_values_2017;
